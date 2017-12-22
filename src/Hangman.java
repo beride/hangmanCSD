dfc3710865e5987823ebe7234a76fb32e2bf5f61
leo <leo@mac.local>
2017-12-22 11:19:02 +0800

    init hangman started

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,14 @@
 public class Hangman {
 
+    public enum status{
+        Started,
+        End
+    }
     public Hangman (String a) {
 
+    }
+
+    public status getStatus() {
+        return status.Started;
     }
 }
--- test/HangmanTest.java
+++ test/HangmanTest.java
@@ -1,9 +1,12 @@
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class HangmanTest {
 
     @Test
     public void when_new_game() {
         Hangman game = new Hangman("");
+        assertEquals(Hangman.status.Started,game.getStatus());
     }
 }
