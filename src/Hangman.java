5b9957993f8f2bdd69868f5fada5b9ce38c9a5f4
leo <leo@mac.local>
2017-12-24 15:41:33 +0800

    test hangman end,won

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,28 +2,49 @@ import com.sun.xml.internal.ws.api.message.Packet;
 
 public class Hangman {
 
+    private Status status;
+    private String answer;
+    private String tried;
+    private int tries;
+
+    public Hangman (String a) {
+
+        answer = a;
+        tries = 0;
+        tried = "";
+        status = Status.Started;
+    }
+
     public String getAnswer() {
-        return "_";
+        return answer;
     }
 
     public String getTried() {
-        return "";
+        return tried;
     }
 
     public int getTries() {
-        return 0;
+        return tries;
+    }
+
+    public void guess(char a) {
+        tried += a;
+        tries++;
+        if(answer.equals (tried))
+        status = Status.Won;
+        else
+            status = Status.End;
     }
 
     public enum Status{
         Started,
-        End
+        Won,
+        End;
     }
-    public Status status;
-    public Hangman (String a) {
 
-    }
+
 
     public Status getStatus() {
-        return Status.Started;
+        return status;
     }
 }
--- test/HangmanTest.java
+++ test/HangmanTest.java
@@ -5,11 +5,32 @@ import static org.junit.Assert.assertEquals;
 public class HangmanTest {
 
     @Test
-    public void when_new_game() {
-        Hangman game = new Hangman("");
-        assertEquals(Hangman.Status.Started,game.getStatus());
-        assertEquals ("_",game.getAnswer());
-        assertEquals ("",game.getTried());
-        assertEquals (0,game.getTries());
+    public void when_new_game_A() {
+        Hangman game = new Hangman("A");
+        assertEquals(Hangman.Status.Started, game.getStatus());
+        assertEquals ("A", game.getAnswer());
+        assertEquals ("", game.getTried());
+        assertEquals (0, game.getTries());
+    }
+    @Test
+    public void given_new_game_A_when_guess_A_then_won() {
+        Hangman game = new Hangman("A");
+        assertEquals(Hangman.Status.Started, game.getStatus());
+        game.guess('A');
+        assertEquals (Hangman.Status.Won, game.getStatus());
+        assertEquals ("A", game.getAnswer ());
+        assertEquals ("A", game.getTried ());
+        assertEquals (1, game.getTries ());
+    }
+
+    @Test
+    public void given_new_game_A_when_guess_B_then_end() {
+        Hangman game = new Hangman("A");
+        assertEquals(Hangman.Status.Started, game.getStatus());
+        game.guess('B');
+        assertEquals (Hangman.Status.End, game.getStatus());
+       // assertEquals ("A", game.getAnswer ());
+        assertEquals ("B", game.getTried ());
+        assertEquals (1, game.getTries ());
     }
 }
