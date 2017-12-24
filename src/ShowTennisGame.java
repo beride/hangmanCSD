ba2aaf2685d8565c47476a1844ff246fdc133422
leo <leo@mac.local>
2017-12-24 17:38:17 +0800

    tennisgame delete history

--- src/ShowTennisGame.java
+++ src/ShowTennisGame.java
@@ -5,32 +5,6 @@ public class ShowTennisGame {
         String wordList[] = {"Love", "Fifteen", "Thirty", "Forty", "All", "Deuce"};
 
         String result = "";
-//        if (playA == 0 && playB == 0){
-//            result = "Love All";
-//        }else if (playA == 1 && playB == 1){
-//            result = "Fifteen All";
-//        }else if (playA == 2 && playB == 2){
-//            result = "Thirty All";
-//        }else if (playA == 3 && playB == 3){
-//            result = "Deuce";
-//        }
-
-//        if (playA - playB == 0) {
-//            switch (playA) {
-//                case 0:
-//                    result = view[playA] + " " + view[3];
-//                    break;
-//                case 1:
-//                    result = view[playA] + " " + view[3];
-//                    break;
-//                case 2:
-//                    result = view[playA] + " " + view[3];
-//                    break;
-//                default:
-//                    result = view[4];
-//                    break;
-//            }
-//        }
 
         int temp = playerA - playerB;
         if (temp == 0){
@@ -42,13 +16,6 @@ public class ShowTennisGame {
         }
 
         if (temp > 0 && playerA < 4 && playerB == 0) {
-//            if (temp == 1) {
-//                result = "Fifteen Love";
-//            } else if (temp == 2) {
-//                result = "Thirty Love";
-//            } else if (temp == 3) {
-//                result = "Forty Love";
-//            }
             result = wordList[temp] + " " + wordList[0];
         } else if (playerA >= 4 && temp >= 2) {
             return "A Win";
